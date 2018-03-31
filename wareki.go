a285552ccb06132e437985dc65550b90015bcc1d
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2018-04-01 00:05:41 +0800

    Applied go fmt

--- wareki.go
+++ wareki.go
@@ -1,57 +1,57 @@
 package wareki
 
 import (
-    "time"
-    "errors"
-    "fmt"
+	"errors"
+	"fmt"
+	"time"
 )
 
 type Wareki struct {
-    Gengo string
-    Year int
-    Date time.Time
+	Gengo string
+	Year  int
+	Date  time.Time
 }
 
 func GetWareki(d time.Time) (string, int, error) {
-    meijiBeginsOn := time.Date(1868, 1, 25, 0, 0, 0, 0, time.UTC)
-    taishoBeginsOn := time.Date(1912, 7, 30, 0, 0, 0, 0, time.UTC)
-    showaBeginsOn := time.Date(1926, 12, 25, 0, 0, 0, 0, time.UTC)
-    heiseiBeginsOn := time.Date(1989, 1, 8, 0, 0, 0, 0, time.UTC)
-    if d.Before(meijiBeginsOn) {
-        return "", -1, errors.New(fmt.Sprintf("%v is invalid date", d))
-    }
-    var gengo string
-    var base int
-    switch {
-        case (d.Equal(meijiBeginsOn) || d.After(meijiBeginsOn)) && d.Before(taishoBeginsOn):
-            gengo = "明治"
-            base = 1868
-        case (d.Equal(taishoBeginsOn) || d.After(taishoBeginsOn)) && d.Before(showaBeginsOn):
-            gengo = "大正"
-            base = 1912
-        case (d.Equal(showaBeginsOn) || d.After(showaBeginsOn)) && d.Before(heiseiBeginsOn):
-            gengo = "昭和"
-            base = 1926
-        case d.Equal(heiseiBeginsOn) || d.After(heiseiBeginsOn):
-            gengo = "平成"
-            base = 1989
-    }
-    year := d.Year() - base + 1
-    return gengo, year, nil
+	meijiBeginsOn := time.Date(1868, 1, 25, 0, 0, 0, 0, time.UTC)
+	taishoBeginsOn := time.Date(1912, 7, 30, 0, 0, 0, 0, time.UTC)
+	showaBeginsOn := time.Date(1926, 12, 25, 0, 0, 0, 0, time.UTC)
+	heiseiBeginsOn := time.Date(1989, 1, 8, 0, 0, 0, 0, time.UTC)
+	if d.Before(meijiBeginsOn) {
+		return "", -1, errors.New(fmt.Sprintf("%v is invalid date", d))
+	}
+	var gengo string
+	var base int
+	switch {
+	case (d.Equal(meijiBeginsOn) || d.After(meijiBeginsOn)) && d.Before(taishoBeginsOn):
+		gengo = "明治"
+		base = 1868
+	case (d.Equal(taishoBeginsOn) || d.After(taishoBeginsOn)) && d.Before(showaBeginsOn):
+		gengo = "大正"
+		base = 1912
+	case (d.Equal(showaBeginsOn) || d.After(showaBeginsOn)) && d.Before(heiseiBeginsOn):
+		gengo = "昭和"
+		base = 1926
+	case d.Equal(heiseiBeginsOn) || d.After(heiseiBeginsOn):
+		gengo = "平成"
+		base = 1989
+	}
+	year := d.Year() - base + 1
+	return gengo, year, nil
 }
 
 func NewWareki(d time.Time) *Wareki {
-    g, y, err := GetWareki(d)
-    if err != nil {
-       return &Wareki{}
-    }
-    return &Wareki{Gengo:g, Year:y, Date:d}
+	g, y, err := GetWareki(d)
+	if err != nil {
+		return &Wareki{}
+	}
+	return &Wareki{Gengo: g, Year: y, Date: d}
 }
 
 func (w *Wareki) String() string {
-    if w.Year == 1 {
-        return fmt.Sprintf("%s%s年%d月%d日", w.Gengo, "元", w.Date.Month(), w.Date.Day())
-    } else {
-        return fmt.Sprintf("%s%d年%d月%d日", w.Gengo, w.Year, w.Date.Month(), w.Date.Day())
-    }
+	if w.Year == 1 {
+		return fmt.Sprintf("%s%s年%d月%d日", w.Gengo, "元", w.Date.Month(), w.Date.Day())
+	} else {
+		return fmt.Sprintf("%s%d年%d月%d日", w.Gengo, w.Year, w.Date.Month(), w.Date.Day())
+	}
 }
--- wareki_test.go
+++ wareki_test.go
@@ -1,128 +1,128 @@
 package wareki
 
 import (
-    "testing"
-    "time"
-    "fmt"
+	"fmt"
+	"testing"
+	"time"
 )
 
 func TestGetWareki01_01(t *testing.T) {
-    d := time.Date(1868, 1, 25, 0, 0, 0, 0, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "明治" {
-        t.Errorf("%s is not 明治", g)
-    }
-    if y != 1 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1868, 1, 25, 0, 0, 0, 0, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "明治" {
+		t.Errorf("%s is not 明治", g)
+	}
+	if y != 1 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki01_02(t *testing.T) {
-    d := time.Date(1912, 7, 29, 23, 59, 59, 999999999, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "明治" {
-        t.Errorf("%s is not 明治", g)
-    }
-    if y != 45 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1912, 7, 29, 23, 59, 59, 999999999, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "明治" {
+		t.Errorf("%s is not 明治", g)
+	}
+	if y != 45 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki02_01(t *testing.T) {
-    d := time.Date(1912, 7, 30, 0, 0, 0, 0, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "大正" {
-        t.Errorf("%s is not 大正", g)
-    }
-    if y != 1 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1912, 7, 30, 0, 0, 0, 0, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "大正" {
+		t.Errorf("%s is not 大正", g)
+	}
+	if y != 1 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki02_02(t *testing.T) {
-    d := time.Date(1926, 12, 24, 23, 59, 59, 999999999, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "大正" {
-        t.Errorf("%s is not 大正", g)
-    }
-    if y != 15 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1926, 12, 24, 23, 59, 59, 999999999, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "大正" {
+		t.Errorf("%s is not 大正", g)
+	}
+	if y != 15 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki03_01(t *testing.T) {
-    d := time.Date(1926, 12, 25, 0, 0, 0, 0, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "昭和" {
-        t.Errorf("%s is not 昭和", g)
-    }
-    if y != 1 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1926, 12, 25, 0, 0, 0, 0, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "昭和" {
+		t.Errorf("%s is not 昭和", g)
+	}
+	if y != 1 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki03_02(t *testing.T) {
-    d := time.Date(1989, 1, 7, 23, 59, 59, 999999999, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "昭和" {
-        t.Errorf("%s is not 昭和", g)
-    }
-    if y != 64 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1989, 1, 7, 23, 59, 59, 999999999, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "昭和" {
+		t.Errorf("%s is not 昭和", g)
+	}
+	if y != 64 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki04_01(t *testing.T) {
-    d := time.Date(1989, 1, 8, 0, 0, 0, 0, time.UTC)
-    g, y, _ := GetWareki(d)
-    if g != "平成" {
-        t.Errorf("%s is not 平成", g)
-    }
-    if y != 1 {
-        t.Errorf("%d is not valid Wareki year", y)
-    }
+	d := time.Date(1989, 1, 8, 0, 0, 0, 0, time.UTC)
+	g, y, _ := GetWareki(d)
+	if g != "平成" {
+		t.Errorf("%s is not 平成", g)
+	}
+	if y != 1 {
+		t.Errorf("%d is not valid Wareki year", y)
+	}
 }
 
 func TestGetWareki05_01(t *testing.T) {
-    d := time.Date(1867, 12, 31, 23, 59, 59, 999999999, time.UTC)
-    _, _, e := GetWareki(d)
-    if e == nil {
-        t.Errorf("%v is valid date", d)
-    } else if fmt.Sprintf("%v", e) != "1867-12-31 23:59:59.999999999 +0000 UTC is invalid date" {
-        t.Errorf("%v", e)
-    }
+	d := time.Date(1867, 12, 31, 23, 59, 59, 999999999, time.UTC)
+	_, _, e := GetWareki(d)
+	if e == nil {
+		t.Errorf("%v is valid date", d)
+	} else if fmt.Sprintf("%v", e) != "1867-12-31 23:59:59.999999999 +0000 UTC is invalid date" {
+		t.Errorf("%v", e)
+	}
 }
 
 func TestGetWareki06_01(t *testing.T) {
-    d := time.Date(2012, 3, 28, 0, 0, 0, 0, time.UTC)
-    w := NewWareki(d)
-    if w.Gengo != "平成" {
-        t.Errorf("%s is not 平成", w.Gengo)
-    }
-    if w.Year != 24 {
-        t.Errorf("%d is not valid Wareki year", w.Year)
-    }
-    if w.String() != "平成24年3月28日" {
-        t.Errorf("%s is not expected date", w.String())
-    }
-    if d != w.Date {
-        t.Errorf("%v is not expected date", w)
-    }
+	d := time.Date(2012, 3, 28, 0, 0, 0, 0, time.UTC)
+	w := NewWareki(d)
+	if w.Gengo != "平成" {
+		t.Errorf("%s is not 平成", w.Gengo)
+	}
+	if w.Year != 24 {
+		t.Errorf("%d is not valid Wareki year", w.Year)
+	}
+	if w.String() != "平成24年3月28日" {
+		t.Errorf("%s is not expected date", w.String())
+	}
+	if d != w.Date {
+		t.Errorf("%v is not expected date", w)
+	}
 }
 
 func TestGetWareki06_02(t *testing.T) {
-    d := time.Date(1926, 12, 26, 0, 0, 0, 0, time.UTC)
-    w := NewWareki(d)
-    if w.Gengo != "昭和" {
-        t.Errorf("%s is not 昭和", w.Gengo)
-    }
-    if w.Year != 1 {
-        t.Errorf("%d is not valid Wareki year", w.Year)
-    }
-    if w.String() != "昭和元年12月26日" {
-        t.Errorf("%s is not expected date", w.String())
-    }
-    if d != w.Date {
-        t.Errorf("%v is not expected date", w)
-    }
+	d := time.Date(1926, 12, 26, 0, 0, 0, 0, time.UTC)
+	w := NewWareki(d)
+	if w.Gengo != "昭和" {
+		t.Errorf("%s is not 昭和", w.Gengo)
+	}
+	if w.Year != 1 {
+		t.Errorf("%d is not valid Wareki year", w.Year)
+	}
+	if w.String() != "昭和元年12月26日" {
+		t.Errorf("%s is not expected date", w.String())
+	}
+	if d != w.Date {
+		t.Errorf("%v is not expected date", w)
+	}
 }
